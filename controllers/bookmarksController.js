fbb6e639b0f9f74153aea592124f06e26c4cf987
stephanie lucano <stephaniemarielucano@gmail.com>
2022-01-11 11:28:56 -0500

    Goal is to get one bookmark by index

--- controllers/bookmarksController.js
+++ controllers/bookmarksController.js
@@ -1,11 +1,13 @@
 const express = require("express")
 
-const bookmarksArray = require("../models/bookmark.js")
+const bookmarksArray = require("../models/bookmarks")
 
 const bookmarks = express.Router()
 
 bookmarks.get("/", (request, response) => {
     response.json(bookmarksArray)
 })
+
+bookmarks.get("/index:", (request, response) => {})
 
 module.exports = bookmarks;
